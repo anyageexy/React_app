53f0eb10c2a245604623f28438a667923eb208f2
anyageexy <anya.alexeeva2001@mail.ru>
2022-06-04 18:54:33 +0500

    доделала форму регистрации

--- my-app/src/components/SignUp.jsx
+++ my-app/src/components/SignUp.jsx
@@ -1,17 +1,27 @@
 import { useDispatch } from "react-redux" //
+import {useNavigate} from 'react-router-dom'     //чтобы сделать переход на главную страницу после регистрации
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { Form } from "./Form"
 import { setUser } from "store/slices/userSlice"
 
 const SignUp = () => {
   
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const push = useNavigate();
 
   const handleRegister = (email, password) => {
     const auth = getAuth();
     console.log(auth);
     createUserWithEmailAndPassword(auth,email, password)
-    .then(console.log)
+    .then(({user}) => {
+      console.log(user);
+      dispatch(setUser({
+        email: user.email,
+        id: user.uid,
+        token: user.accessToken,
+      }));
+      push('/');
+    })
     .catch(console.error);
     
   } //Это будет функция при вызове которой я ожидаю
