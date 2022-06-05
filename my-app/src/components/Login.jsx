026ff0229e3b54d922418d182e8a492798633665
anyageexy <anya.alexeeva2001@mail.ru>
2022-06-05 16:48:46 +0500

    закончила авторизацию для приложения

--- my-app/src/components/Login.jsx
+++ my-app/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import { useDispatch } from "react-redux" //
+import {useNavigate} from 'react-router-dom'    
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Form } from "./Form"
 import { setUser } from "store/slices/userSlice"
@@ -6,12 +7,20 @@ import { setUser } from "store/slices/userSlice"
 const Login = () => {
   
   const dispatch = useDispatch()
-
+  const push = useNavigate();
   const handleLogin = (email, password) => {
     const auth = getAuth();
-    signInWithEmailAndPassword(auth,email, password)
-    .then(console.log)
-    .catch(console.error);
+    signInWithEmailAndPassword(auth, email, password)
+    .then(({user}) => {
+      console.log(user);
+      dispatch(setUser({
+        email: user.email,
+        id: user.uid,
+        token: user.accessToken,
+      }));
+      push('/');
+    })
+    .catch(() => alert('Такого аккаунта не существует'));
     
   } //Это будет функция при вызове которой я ожидаю
   //что у четные данные из формы отправятся на сервер и я получу нужного пльзователя
--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
-import {useNavigate} from 'react-router-dom'
+import { useDispatch } from "react-redux";
+// import {useNavigate} from 'react-router-dom'
+import {useAuth} from 'hooks/use-auth'
+import { removeUser } from "store/slices/userSlice";
 
 const HomePage = () => {
-  return(
+  const dispatch = useDispatch();
+  const {isAuth, email} = useAuth();
+  return isAuth ? (
     <div>
-      <h1>Home page</h1>
-      <Link to= '/login'>Войдите в систему</Link>
+      <h1>Добро пожаловать</h1>
+      <button onClick = {() => dispatch(removeUser())}>Выйти из аккаунта {email}</button>
     </div>
-    
+  ) : 
+  (
+      <Link to= '/login'>Войдите в систему</Link>
   )
 }
 
